31a7e9e72a8ef375d75fc8f682ec55816c0df447
sai sharan <sairavi1999@gmail.com>
2025-10-30 20:29:49 +0100

    intial changes

--- currency_conv_api.py
+++ currency_conv_api.py
@@ -1,13 +1,20 @@
 import requests
+import os 
+from dotenv import load_dotenv
+
+load_dotenv() 
+
+api_key = os.getenv("CURRENCY_API_KEY")
+
 
 def convert_currency(amount, from_currency="EUR", to_currency="INR"):
     # Your API access key
-    access_key = "15dd497a3197675fa5d1bdf2e4bad0e6"
+    access_key = api_key
     url = "https://api.exchangerate.host/convert?from=EUR&toINR&amount=100"
 
     # Define the parameters
     params = {
-        "access_key": access_key,
+        "access_key": api_key,
         "from": from_currency,
         "to": to_currency,
         "amount": amount
@@ -16,15 +23,17 @@ def convert_currency(amount, from_currency="EUR", to_currency="INR"):
     # Send GET request to CurrencyLayer API
     response = requests.get(url, params=params)
     data = response.json()
-    print(data)
+
 
     # Check if API call is successful
     if data.get("success"):
         result = data["result"]
-        print(f"{amount} {from_currency} = {result:.2f} {to_currency}")
+        print(f"The total expenditure equivallent {amount} {from_currency} = {result:.2f} {to_currency}")
         return result
     else:
         print("Error:", data.get("error", {}).get("info", "Unknown error"))
         return None
+
+    print(data)
 
 
